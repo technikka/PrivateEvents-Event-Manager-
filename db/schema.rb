271767e26e1d0c02b8a404c64df5303949c386e4
Pythonikk <nikkibrusoe@gmail.com>
2022-05-09 13:04:36 -0700

    Working association between events which has a creator and User class

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_09_171252) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_09_195909) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "location"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_171252) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
