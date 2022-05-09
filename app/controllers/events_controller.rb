b84398f8dff83cb0e0d7cbf087652f58c72c5b8b
Pythonikk <nikkibrusoe@gmail.com>
2022-05-09 15:12:39 -0700

    Create form for new event, creators page and index link to event's show.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,29 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.all.order(:date)
+  end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to event_path(@event.id), notice: 'Event was successfully created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :date)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+
+  validates :creator_id, presence: true
+  validates :date, presence: true
+  validates :location, presence: true
+  validates :title, presence: true
 end
