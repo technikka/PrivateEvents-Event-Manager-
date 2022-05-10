7ae0f90ca0be8140268de31d616d97dc3bc32046
Pythonikk <nikkibrusoe@gmail.com>
2022-05-09 17:35:04 -0700

    Fix redirect for new logins and sign ups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # methods for devise redirect after successful sign in or sign up.
+  # sometimes these methods need to be in a RegistrationsController which inherits from Devise::RegistrationsController
+  # ^^ no idea why sometimes one works, sometimes the other.
+
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
 end
--- app/controllers/registrations_controller.rb
+++
@@ -1,5 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-  def after_sign_up_path_for(resources)
-    new_user_session_path
-  end
-end
